de654520797fdc0c5088e303f1bad57fd017b8b9
Urtera <umutruzgaroztekin@gmail.com>
2026-01-23 16:32:12 +0300

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -15,11 +15,16 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
+import edu.wpi.first.math.MathUtil;
+
 
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 
 public class RobotContainer {
+    public double hedef = 0.0;
+    public boolean basildi = false;
+    public double hiz = 0.0;
     private double MaxSpeed = 1.0 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
     private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
 
@@ -41,42 +46,66 @@ public class RobotContainer {
     }
 
     private void configureBindings() {
-        // Note that X is defined as forward according to WPILib convention,
-        // and Y is defined as to the left according to WPILib convention.
-        drivetrain.setDefaultCommand(
-            // Drivetrain will execute this command periodically
-            drivetrain.applyRequest(() ->
-                drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
-                    .withVelocityY(-joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
-                    .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
-            )
-        );
-
-        // Idle while the robot is disabled. This ensures the configured
-        // neutral mode is applied to the drive motors while disabled.
         final var idle = new SwerveRequest.Idle();
         RobotModeTriggers.disabled().whileTrue(
             drivetrain.applyRequest(() -> idle).ignoringDisable(true)
         );
 
-        joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
-        joystick.b().whileTrue(drivetrain.applyRequest(() ->
-            point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
-        ));
-
-        // Run SysId routines when holding back/start and X/Y.
-        // Note that each routine should be run exactly once in a single log.
-        joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-        joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
-        joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
-        joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
-
-        // Reset the field-centric heading on left bumper press.
+        joystick.y().onTrue(Commands.runOnce(() -> 
+        {
+            hedef = 0.0;
+            basildi = true;
+        }));
+        joystick.a().onTrue(Commands.runOnce(() ->
+        {
+            hedef = Math.PI;
+            basildi = true;
+        }));
+        joystick.x().onTrue(Commands.runOnce(() ->
+        {
+            hedef = -Math.PI/2;
+            basildi = true;
+        }));
+        joystick.b().onTrue(Commands.runOnce(() ->
+        {
+            hedef = Math.PI/2;
+            basildi = true;
+        }));
+         drivetrain.setDefaultCommand(
+			drivetrain.applyRequest(() -> 
+            {
+                if(Math.abs(joystick.getRightX()) > 0.1) 
+                {
+                    basildi = false;
+                }   
+                if(basildi)
+                {
+                    double hata = Rotation2d.fromRadians(hedef)
+	                                        .minus(drivetrain.getState().Pose.getRotation())
+	                                        .getRadians();
+                    hata = Math.atan2(Math.sin(hata), Math.cos(hata));
+                    if(Math.abs(hata) < Math.toRadians(3))
+                    {
+                        basildi = false;
+                        hiz = 0;
+                    }
+                    else
+                    {
+                        	hiz = hata * 3;
+	                        hiz = MathUtil.clamp(hiz, -MaxAngularRate, MaxAngularRate);
+                    }
+                }
+                else
+                {
+                    hiz = -joystick.getRightX() * MaxAngularRate;
+                }
+                return drive.withVelocityX(-joystick.getLeftY() * MaxSpeed)
+					.withVelocityY(-joystick.getLeftX() * MaxSpeed)
+					.withRotationalRate(hiz);
+            }));
         joystick.leftBumper().onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
-
         drivetrain.registerTelemetry(logger::telemeterize);
     }
-
     public Command getAutonomousCommand() {
         // Simple drive forward auton
         final var idle = new SwerveRequest.Idle();
